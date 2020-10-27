ce4459afc24eab233a4e2e5038eabe2c136da7a5
Juan <juanalexandro.roblesaraiza@gmail.com>
2020-10-26 19:49:02 -0600

    casas
    dsdscd

--- PT-API-P/admin/casas/cuartos/desactivarcuarto.php
+++ PT-API-P/admin/casas/cuartos/desactivarcuarto.php
@@ -2,7 +2,7 @@
     require("../../../headers.php");
     require("../../../conexion.php");
     $conexion = conexion();
-    $id_casa = $_GET['id_cuarto'];
+    $id_cuarto = $_GET['id_cuarto'];
     $consulta = "UPDATE cuarto SET estado_cuarto = '0' WHERE id_cuarto = '$id_cuarto'";
     mysqli_query($conexion, $consulta) or die(mysqli_error($conexion));
 ?>
