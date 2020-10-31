e1e01dc231a0c17fe043a801cd0a7e0b93b76bf7
Dario Menendez <dariomego@gmail.com>
2020-10-31 13:59:59 -0600

    cambio de nombre ahora si

--- C-API-P/conexion.php
+++ C-API-P/conexion.php
@@ -2,10 +2,15 @@
 
   function conexion() {
 
+    // $dbServername = "localhost";
+    // $dbUsuario = "u432981095_root";
+    // $dbContra = "9=OgaJoPsc";
+    // $dbName = "u432981095_casas";
+
     $dbServername = "localhost";
-    $dbUsuario = "u432981095_root";
-    $dbContra = "9=OgaJoPsc";
-    $dbName = "u432981095_casas";
+    $dbUsuario = "root";
+    $dbContra = "";
+    $dbName = "casas";
     
     $conexion = mysqli_connect($dbServername, $dbUsuario, $dbContra, $dbName);
     mysqli_set_charset($conexion,'utf8');
