d7796d6f1783ee717d41858c1fd1b713ba992866
Juan <juanalexandro.roblesaraiza@gmail.com>
2020-11-01 23:08:50 -0600

    Update verCuartosCasa.php

--- C-API-P/cliente/casas/Cuartos/verCuartosCasa.php
+++ C-API-P/cliente/casas/Cuartos/verCuartosCasa.php
@@ -5,7 +5,7 @@
     $conexion = conexion();
 
     $registros = mysqli_query($conexion, "SELECT *FROM cuarto WHERE fk_casa=$_GET[id_casa] AND estado_cuarto = '1'");
-    if(mysqli_num_rows($registros) > 1){
+    if(mysqli_num_rows($registros) > 0){
         $cuartos = [];
 
         while ($resultado = mysqli_fetch_array($registros)){
