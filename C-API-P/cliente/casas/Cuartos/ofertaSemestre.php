6d5eb245944f1d5e6248ee1a329bc40949423afb
Juan <juanalexandro.roblesaraiza@gmail.com>
2020-11-01 23:15:43 -0600

    Update ofertaSemestre.php

--- C-API-P/cliente/casas/Cuartos/ofertaSemestre.php
+++ C-API-P/cliente/casas/Cuartos/ofertaSemestre.php
@@ -22,14 +22,14 @@
         if($compra == null){
             $datos_compra_completos['tipo'] = 1;
         }else{
-            $usuario = Usuario::DatosUsuarioid($compra['fk_usuario']);
-            if($compra['pago'] == 1){
+            $usuario = Usuario::DatosUsuarioid($compra[0]['fk_usuario']);
+            if($compra[0]['pago'] == 1){
                 $datos_compra_completos['pago'] = "Pagado";
             }else{
                 $datos_compra_completos['pago'] = "No se ha confirmado el pago";
             }
-            $datos_compra_completos['foto'] = $usuario['foto'];
-            $datos_compra_completos['nombre_usuario'] = $usuario['nombre_usuario'];
+            $datos_compra_completos['foto'] = $usuario[0]['foto'];
+            $datos_compra_completos['nombre_usuario'] = $usuario[0]['nombre_usuario'];
             $datos_compra_completos['tipo'] = 2;
         }
     }
