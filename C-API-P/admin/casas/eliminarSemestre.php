833b8cbb5a58479df69ecdc1c3ee74065a14e3d7
Juan <juanalexandro.roblesaraiza@gmail.com>
2020-11-08 14:12:15 -0600

    cambio

--- C-API-P/admin/casas/eliminarSemestre.php
+++ C-API-P/admin/casas/eliminarSemestre.php
@@ -3,7 +3,7 @@
     require("../../BD.php");
     require("../../modelo/ClaseChat.php");
     $id_semestre = $_GET['id_semestre'];
-    $consulta = "SELECT FROM oferta WHERE fk_semestre = '$id_semestre'";
+    $consulta = "SELECT *FROM oferta WHERE fk_semestre = '$id_semestre'";
     $resultado = BD::consultaSelect($consulta);
     if(mysqli_num_rows($resultado) > 0){
         $respuesta = false;
