2e7c4e28f796a088840e1539cb12344afb576a7b
Juan <juanalexandro.roblesaraiza@gmail.com>
2020-11-07 21:35:36 -0600

    ya

--- C-API-P/admin/casas/cuartos/modificarOferta.php
+++ C-API-P/admin/casas/cuartos/modificarOferta.php
@@ -1,9 +1,9 @@
 <?php
     require("../../../headers.php");
     require("../../../conexion.php");
-    $id_oferta = $_GET[''];
-    $grupo = $_GET[''];
-    $precio = $_GET[''];
+    $id_oferta = $_GET['id_oferta'];
+    $grupo = $_GET['grupo'];
+    $precio = $_GET['precio'];
     $consulta = "UPDATE oferta SET precio = '$precio', grupo = '$grupo' WHERE id_oferta = '$id_oferta'";
     $registro = mysqli_query($conexion,$consulta) or die(mysqli_error($conexion));
     echo json_encode($mensajes); 
--- C-API-P/admin/casas/insertaSemestre.php
+++ C-API-P/admin/casas/insertaSemestre.php
@@ -2,9 +2,9 @@
     require("../../../headers.php");
     require("../../../BD.php");
     require("../../../modelo/ClaseChat.php");
-    $inicio = $_POST[''];
-    $fin = $_POST[''];
-    $nombre = $_POST[''];
+    $inicio = $_POST['inicio'];
+    $fin = $_POST['fin'];
+    $nombre = $_POST['nombre'];
     $consulta = "INSERT INTO semestre(nombre, inicio_semestre, fin_semestre) VALUES ('$nombre','$inicio','$fin')";
     $resultado = BD::consultaSelect($consulta);
     echo json_encode($mensajes); 
