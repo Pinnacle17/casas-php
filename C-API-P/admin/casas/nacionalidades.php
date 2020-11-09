26dc55ea434092a1d48796121b5c8dd8ccc2a8bb
jessusu20 <c.f.cubers@gmail.com>
2020-11-08 19:06:51 -0600

    NACIONALIDADES CORREGIDO

--- C-API-P/admin/casas/nacionalidades.php
+++ C-API-P/admin/casas/nacionalidades.php
@@ -1,12 +1,14 @@
 <?php
     require("../../headers.php");
     require("../../BD.php");
-    $consulta = "SELECT usuario.nacionalidad, COUNT(usuario.nacionalidad) AS numero FROM compra JOIN usuario ON compra.fk_usuario=usuario.id_usuario GROUP BY usuario.nacionalidad ORDER BY numero";
+    $consulta = "SELECT usuario.nacionalidad, COUNT(usuario.nacionalidad) AS numero FROM compra JOIN usuario ON compra.fk_usuario=usuario.id_usuario GROUP BY usuario.nacionalidad ORDER BY numero DESC";
     $resultado = BD::consultaSelect($consulta);
     if(mysqli_num_rows($resultado) > 0){
-        while ($row = mysqli_fetch_row($resultado)) {
-            $resultado1["cantidad"] = $row[1];
-            $resultado1["nacionalidad"] = $row[0];
+        $x=0;
+        while ($row = mysqli_fetch_array($resultado)) {
+            $resultado1[$x]["cantidad"] = $row[1];
+            $resultado1[$x]["nacionalidad"] = $row[0];
+            $x++;
         }
     }else{
         $resultado = null;
