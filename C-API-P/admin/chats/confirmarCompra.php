5df94c032802b98027336a7d73843c5d82856662
Juan <juanalexandro.roblesaraiza@gmail.com>
2020-11-08 20:06:26 -0600

    Update confirmarCompra.php

--- C-API-P/admin/chats/confirmarCompra.php
+++ C-API-P/admin/chats/confirmarCompra.php
@@ -8,7 +8,7 @@
     $fecha = date("Y-m-d H:i:s"); 
     $consulta_insert_compra = "INSERT INTO compra(pago, fecha_compra, fk_oferta, fk_usuario) VALUES ('0', '$fecha','$id_oferta', '$id_usuario')";
     BD::consultaSelect($consulta_insert_compra);
-    Chat::UpdateNotificacion($id_chat, 4);
+    //Chat::UpdateNotificacion($id_chat, 4);
     Usuario::modificarEstadoOferta($id_oferta, 0);
     Usuario::EstadoUsuario($id_usuario, 3);
 
